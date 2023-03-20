0dccac1d59c9776b584a9f656c409f363980da6c
Mircea-Gelu Egry <mirceaegry72@gmail.com>
2023-03-20 17:22:33 +0200

    finished searchbar

--- myfriends-client/src/components/SearchBar.js
+++ myfriends-client/src/components/SearchBar.js
@@ -3,36 +3,34 @@ import {
   FormControl,
   IconButton,
   InputAdornment,
+  Modal,
   TextField,
 } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
 import { COLORS } from "../values/colors";
 
-const SearchBar = () => {
-  const handleSearchClick = () => {};
-
+const SearchBar = ({ barWidth, handleOnChange }) => {
   return (
     <FormControl variant="filled" sx={{ width: "100%", alignItems: "center" }}>
       <TextField
         id="password-field"
         variant="outlined"
         size="normal"
-        sx={{ width: "50%", backgroundColor: COLORS.white }}
+        sx={{ width: `${barWidth}`, backgroundColor: COLORS.white }}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">Search</InputAdornment>
           ),
           endAdornment: (
             <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                edge="end"
-                onClick={handleSearchClick}
-              >
+              <IconButton aria-label="toggle password visibility" edge="end">
                 <Search />
               </IconButton>
             </InputAdornment>
           ),
         }}
+        onChange={handleOnChange}
       />
     </FormControl>
   );
--- myfriends-client/src/pages/content/Home.js
+++ myfriends-client/src/pages/content/Home.js
@@ -1,13 +1,49 @@
-import { Grid } from "@mui/material";
+import { Avatar, Divider, Grid, List, Paper, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Post from "../../components/Post";
 import SearchBar from "../../components/SearchBar";
 import authHeader from "../../services/authentication/auth-header";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+  const [displayContent, setDisplayContent] = useState("");
+  const [displaySearch, setDisplaySearch] = useState("none");
+
+  const config = {
+    headers: authHeader(),
+  };
+
+  const navigate = useNavigate();
+
+  const handleUserProfile = (user) => {
+    navigate(`/${user}`);
+  };
+
+  const handleOnChange = (event) => {
+    if (event.target.value.length > 3) {
+      axios
+        .get(
+          process.env.REACT_APP_BASE_API_URL + `search/${event.target.value}`,
+          config
+        )
+        .then((response) => {
+          if (response.data.length > 0) {
+            setDisplayContent("none");
+            setDisplaySearch("");
+          }
+
+          setSearchResult(response.data);
+          console.log(response.data);
+        });
+    } else {
+      setDisplayContent("");
+      setSearchResult([]);
+    }
+  };
 
   useEffect(() => {
     const config = {
@@ -26,12 +62,62 @@ const Home = () => {
   return (
     <Grid container>
       <Stack sx={{ width: "100%", alignItems: "center" }} spacing={5}>
-        <SearchBar />
-        <Stack sx={{ width: "35%" }} spacing={3}>
+        <SearchBar barWidth="50%" handleOnChange={handleOnChange} />
+
+        <Stack sx={{ width: "35%" }} spacing={3} display={displayContent}>
           {posts.map((post) => {
             return <Post key={post.post.id} postEntity={post} />;
           })}
         </Stack>
+
+        <Paper
+          sx={{
+            width: "55%",
+            maxHeight: "80vh",
+            overflow: "auto",
+            "&::-webkit-scrollbar": {
+              width: "5px",
+            },
+            "&::-webkit-scrollbar-track": {
+              borderRadius: "23px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              background: "gray",
+              borderRadius: "10px",
+            },
+            padding: "20px",
+          }}
+          elevation={3}
+          display={displaySearch}
+        >
+          <List sx={{ padding: "20px" }}>
+            {searchResult.map((user) => {
+              return (
+                <Paper
+                  key={user.id}
+                  elevation={2}
+                  sx={{ padding: "20px", marginBottom: 1 }}
+                >
+                  <Stack direction={"row"} justifyContent={"space-between"}>
+                    <Stack
+                      direction={"row"}
+                      sx={{ alignItems: "center", cursor: "pointer" }}
+                      spacing={2}
+                      onClick={() => {
+                        handleUserProfile(user.username);
+                      }}
+                    >
+                      <Avatar></Avatar>
+                      <Typography>
+                        <b>{user.username}</b>
+                      </Typography>
+                    </Stack>
+                  </Stack>
+                </Paper>
+              );
+            })}
+          </List>
+        </Paper>
       </Stack>
     </Grid>
   );
